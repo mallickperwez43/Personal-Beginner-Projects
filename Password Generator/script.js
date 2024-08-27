197b984fd372557a921a2e29aa238d964082849a
Perwez Mallick <mallickperwez43@gmail.com>
2024-08-27 13:16:41 +0530

    Update script.js

--- Password Generator/script.js
+++ Password Generator/script.js
@@ -1,32 +1,32 @@
 let lenEl = document.getElementById("len");
 lenEl.value = "0";
 
-function decrement(){
+function decrement() {
     let val = Number.parseInt(lenEl.value);
-    if(val > 0){
+    if (val > 0) {
         val--;
     }
     lenEl.value = val.toString();
 }
 
-function increment(){
+function increment() {
     let val = Number.parseInt(lenEl.value);
-    if(val >= 0){
+    if (val >= 0) {
         val++;
     }
     lenEl.value = val.toString();
 }
 
-const arr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", ";", ":", "\"", "'", "<", ">", ",", ".", "?", "/", "\\", "|", "~", "`", "1", "2", "3", "4", "5", "6", "7", "8", "9","0"];
+const arr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", ";", ":", "\"", "'", "<", ">", ",", ".", "?", "/", "\\", "|", "~", "`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 let arrLength = arr.length;
 
 let passwordEl = document.getElementById("pass");
 
-function generatePassword(){
+function generatePassword() {
     let length = Number.parseInt(lenEl.value);
-    
+
     let pass = "";
-    for(let i = 0; i < length; i++){
+    for (let i = 0; i < length; i++) {
         pass += arr[Math.floor(Math.random() * arrLength)];
     }
     passwordEl.value = pass;
